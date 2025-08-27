813fe4055a9a6998bb70e97a7c49541c0103af6d
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2025-08-27 19:49:24 +0200

    Changed Github Action to create the repo
    - The ALCI repository now is down/not-working. Removed.
    - Some versions are deprecated on the action, upgraded.
    - Changed to get yay-bin to use AUR packages.
    - Manjaro pkgbuild to compile Calamares for my custom repository.
    - To compile Calamares, some dependencies are needed. ckbcomp can't be compiled in a container. Using chaotic-aur mirror to get it.
    - We need the ckbomp package in the repository in order to install then to create the ISO.

--- repo/create_repo.sh
+++ repo/create_repo.sh
@@ -6,10 +6,9 @@
 #
 ###########
 
-cd repo
-
 PACKAGES=(
     "https://aur.archlinux.org/yay-bin.git yay-bin"
+    "https://gitlab.manjaro.org/packages/extra/calamares.git calamares"
     "https://github.com/yuki/miladi-calamares-config miladi-calamares-config"
     "https://github.com/yuki/miladi-live-service.git miladi-live-service"
 )
@@ -23,17 +22,43 @@ create_package() {
     chmod 777 $name
     runuser -unobody git clone $url
     cd $name
+    source PKGBUILD && yay -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}"
     runuser -unobody makepkg
 
-    repo-add  ../miladi.db.tar.gz *pkg.tar*
     mv *pkg.tar* ../
 
     cd ..
     rm -fr $name
 }
 
-pacman -Sy --noconfirm git base-devel
+
+cd repo
+
+# added chaotic-repo for some dependencies https://aur.chaotic.cx/docs
+pacman -Sy
+pacman-key --init
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+# added installation for yay-bin
+pacman -Sy --noconfirm --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+chmod 777 yay-bin
+cd yay-bin
+runuser -unobody makepkg
+pacman -U --noconfirm *pkg.tar*
+cd ..
+
 
 for value in "${PACKAGES[@]}"; do
   create_package $value
 done
+
+
+# we need the package ckbcomp in the repository
+cp /var/cache/pacman/pkg/ckbcomp*zst .
+repo-add  miladi.db.tar.gz *pkg.tar.zst
